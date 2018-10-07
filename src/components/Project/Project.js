5805ed3a4d59e74486c2b4172f50426c427dbcb1
Maham Shahid <mahum.shahid934@gmail.com>
2018-10-08 02:40:12 +0500

    Issue#28: Redesigned project page

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
-import Typography from '@material-ui/core/Typography';
+
 import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Chip from '@material-ui/core/Chip';
+
 import AppService from '../../services/AppService';
+import './Project.css';
+
+
+const getLinkElement = (linkName, linkValue) => {
+  if (linkValue !== '') {
+    return (
+      <a
+        className={`link-item ${linkName}`}
+        href={linkValue}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {linkName}
+      </a>
+    );
+  }
+}
 
 export default class Project extends Component {
   constructor(props) {
@@ -25,70 +46,45 @@ export default class Project extends Component {
     const { project } = this.state;
 
     return (
-      <Grid container justify="center" spacing={0}>
-        <Grid item xs={12}>
-          <br />
-          <Typography
-            styles={{ marginTop: '20px' }}
-            variant="display1"
-            align="center"
-            component="h1"
-            gutterBottom
-          >
-            {project ? project.name : ''}
-          </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <Typography variant="subheading" className="description" align="center" paragraph>
-            {project ? project.description : ''}
-          </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <Typography className="description" align="center" paragraph>
-            <b>Tech Stack: </b>
-            {project ? project.tech : ''}
-          </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <Typography className="description" align="center" paragraph>
-            <b>Looking for partners: </b>
-            {project ? project.partners : ''}
-          </Typography>
-        </Grid>
-        <Grid item xs={12} align="center">
-          {project && project.link ? (
-            <Button
-              href={project.link}
-              style={{ backgroundColor: '#ff0000b3', color: 'white', margin: '10px' }}
-              target="_blank"
-            >
-              Website
-            </Button>
-          ) : (
-            ''
-          )}
-          {project && project.chatLink ? (
-            <Button
-              href={project.chatLink}
-              style={{ backgroundColor: '#008000b3', color: 'white', margin: '10px' }}
-              target="_blank"
-            >
-              Slack/Gitter
-            </Button>
-          ) : (
-            ''
-          )}
-          {project && project.repoLink ? (
-            <Button
-              href={project.repoLink}
-              style={{ backgroundColor: '#0072ffb3', color: 'white', margin: '10px' }}
-              target="_blank"
-            >
-              Repo
-            </Button>
-          ) : (
-            ''
-          )}
+      <Grid container spacing={0}>
+        <Grid item container xs={12} spacing={0}>
+          <Card className="card">
+            <CardContent>
+              <Typography className="title" variant="title" component="h1">
+                {project ? project.name : ''}
+              </Typography>
+              <Typography className="links-container" component="div">
+                {getLinkElement('Website', project && project.link ? project.link : '')}
+                {getLinkElement('Repo', project && project.repoLink ? project.repoLink : '')}
+                {getLinkElement('Chat', project && project.chatLink ? project.chatLink : '')}
+              </Typography>
+              <Typography className="description" component="p">
+                {project ? project.description : ''}
+              </Typography>
+              <Typography className="tech-container" component="div">
+                <Typography className="tech-title" component="h2">
+                  {'Tech Stack'}
+                </Typography>
+                <Chip
+                  label={project && project.tech ? project.tech : ''}
+                  className="chip-divider"
+                  color="secondary"
+                />
+              </Typography>
+              <Typography className="resources-container" component="div">
+                <Typography className="resources-title" component="h2">
+                  {'Resources Needed'}
+                </Typography>
+                <div className="resource-items">
+                  <Chip
+                    label={project && project.partners ? project.partners : ''}
+                    className="chip-divider"
+                    color="primary"
+                  />
+                </div>
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
       </Grid>
     );
